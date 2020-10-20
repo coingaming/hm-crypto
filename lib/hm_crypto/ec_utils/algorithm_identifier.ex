b1b97ab0a7601d35fac2df7b8c5e60316d9bae25
ilia.shchepin <ilia.shchepin@heathmont.net>
2020-10-20 12:20:39 +0300

    Add more types; update dialyzer config

--- lib/hm_crypto/ec_utils/algorithm_identifier.ex
+++ lib/hm_crypto/ec_utils/algorithm_identifier.ex
@@ -6,4 +6,10 @@ defmodule HmCrypto.EcUtils.AlgorithmIdentifier do
     :AlgorithmIdentifier,
     Record.extract(:AlgorithmIdentifier, from_lib: "public_key/include/OTP-PUB-KEY.hrl")
   )
+
+  @type t ::
+          record(:record,
+            algorithm: any(),
+            parameters: any()
+          )
 end
--- lib/hm_crypto/ec_utils/subject_public_key_info.ex
+++ lib/hm_crypto/ec_utils/subject_public_key_info.ex
@@ -6,4 +6,10 @@ defmodule HmCrypto.EcUtils.SubjectPublicKeyInfo do
     :SubjectPublicKeyInfo,
     Record.extract(:SubjectPublicKeyInfo, from_lib: "public_key/include/OTP-PUB-KEY.hrl")
   )
+
+  @type t ::
+          record(:record,
+            algorithm: HmCrypto.EcUtils.AlgorithmIdentifier.t(),
+            subjectPublicKey: binary()
+          )
 end

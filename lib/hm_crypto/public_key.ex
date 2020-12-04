cecb8b429316c425ee13b62f8b1990916f8e3e3f
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2020-12-04 18:00:28 +0200

    Add func to convert RSA tuple to PEM

--- lib/hm_crypto/public_key.ex
+++ lib/hm_crypto/public_key.ex
@@ -21,4 +21,20 @@ defmodule HmCrypto.PublicKey do
   end
 
   def parse_pem(pem) when is_tuple(pem), do: pem
+
+  @doc """
+
+  Function encodes tuple representation of RSA key to PEM format (string).
+  If string is given as argument - returns it as is.
+
+  """
+
+  @spec encode_pem(rsa_key()) :: String.t
+  def encode_pem(rsa_key) when is_tuple(rsa_key) do
+    key_type = elem(rsa_key, 0)
+    pem_entry = :public_key.pem_entry_encode(key_type, rsa_key)
+    :public_key.pem_encode([pem_entry])
+  end
+
+  def encode_pem(rsa_key) when is_binary(rsa_key), do: rsa_key
 end
